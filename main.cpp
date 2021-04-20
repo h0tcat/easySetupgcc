9bb553d03f5fda344af15e725ba8174ac79829ff
h0tcat <h0tnori@vivaldi.net>
2021-04-20 18:22:50 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main(void){
+    std::cout << "gccをインストールするために必要なプログラムをインストールします。" << std::endl;
+    system("start powershell \"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager/::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'))\"");
+    std::cout << "OK." << std::endl;
+    std::cout << "gccをインストールします。" << std::endl;
+    system("choco install mingw");
+
+    std::cout << "インストールが完了しました!" << std::endl;
+    std::cout << "一度プログラムを終了して、コマンドプロンプトを開き、gccコマンドが動作するかを確認してください。" << std::endl;
+    std::cout << "enterキーを押すと終了します" << std::endl;
+    std::string buffer;
+    std::cin >>  buffer;
+    exit(0);
+    return 0;
+}
